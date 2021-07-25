0e3ff9e6382119e8e144c1f5da06c8b1b1ae56f9
Abraham Arslan <arslan@abrahamarslan.com>
2021-07-25 13:54:01 +0530

    #13 - Project Seven

--- js/project-seven.js
+++ js/project-seven.js
@@ -1,4 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
+//Section two - image animation
 gsap.to('.image-single', {
     scale: 1,
     duration: 1,
@@ -6,8 +7,38 @@ gsap.to('.image-single', {
    scrollTrigger: {
         trigger: '#section-2',
         start: 'top 100%',
-        end: 'bottom 100%',
-        markers: true,
+        end: 'bottom 60%', // Because we want to end animation while the content is at viewer's screen not after he has reached the end.
+        // markers: true,
         scrub: true
    }
 });
+//Now trigger the staggered animation for the text items
+gsap.to('.text-item', {
+    duration: 1,
+    opacity: 1,
+    stagger: 0.5,
+    ease: 'sine.in',
+    scrollTrigger: {
+        trigger: '#section-3',
+        start: 'top-80%',
+        end: 'center 40%',
+        scrub: true
+    }
+});
+
+//Now animate the sprite image
+gsap.to('.sprite', {
+    scrollTrigger: {
+        trigger: '#section-4',
+        scrub: true,
+        start: 'top 80%',
+        onEnter: animateSprite,
+        markers: true
+    }
+});
+
+//Define the function which handles the animation triggered by scroll
+function animateSprite() {
+    console.log('in');
+    document.querySelector('.sprite').style.animation = 'dance 1.5s steps(6) alternate'
+}
